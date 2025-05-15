aef397d2574f5fb5dd3b2d20fc0f1fda334f42c2
German Shteynardt <gvshteynardt@gmail.com>
2025-05-15 12:40:36 +0300

    feat: added FixedCapacityQueue

--- data-structures/FixedCapacityQueue.js
+++ data-structures/FixedCapacityQueue.js
@@ -1,27 +1,47 @@
+const assert = (e) => {
+    if (!e) {
+        throw new Error('assertion failed');
+    }
+};
+
 class FixedCapacityQueue {
     constructor(maxSize) {
+        assert(maxSize > 0);
+
         this.q = Array.from({ length: maxSize }, () => null);
-        this.after = 0;
+        this.last = maxSize - 1;
         this.first = 0;
-        this.size = 0;
+        this._size = 0;
     }
 
-    push(value) {
-        assert(this.size < this.q.length);
-        this.size++;
+    peekFirst() {
+        assert(this._size > 0);
 
-        this.q[this.after] = value;
-        this.after++;
+        return this.q[this.first];
+    }
 
-        if (this.after === this.q.length) {
-            this.after = 0;
+    peekLast() {
+        assert(this._size > 0);
+        return this.q[this.last];
+    }
+
+    addLast(value) {
+        assert(this._size < this.q.length);
+        this._size++;
+
+        this.last++;
+
+        if (this.last === this.q.length) {
+            this.last = 0;
         }
+
+        this.q[this.last] = value;
     }
 
-    poll() {
+    pollFirst() {
         assert(!this.isEmpty());
 
-        this.size--;
+        this._size--;
 
         const value = this.q[this.first];
         this.first++;
@@ -34,6 +54,10 @@ class FixedCapacityQueue {
     }
 
     isEmpty() {
-        return this.size === 0;
+        return this._size === 0;
+    }
+
+    size() {
+        return this._size;
     }
 }
---
+++ data-structures/FixedCapacityQueueSimple.js
@@ -0,0 +1,45 @@
+const assert = (e) => {
+    if (!e) {
+        throw new Error('assertion failed');
+    }
+};
+
+class FixedCapacityQueue {
+    constructor(maxSize) {
+        this.q = Array.from({ length: maxSize }, () => null);
+        this.after = 0;
+        this.first = 0;
+        this.size = 0;
+    }
+
+    push(value) {
+        assert(this.size < this.q.length);
+        this.size++;
+
+        this.q[this.after] = value;
+        this.after++;
+
+        if (this.after === this.q.length) {
+            this.after = 0;
+        }
+    }
+
+    poll() {
+        assert(!this.isEmpty());
+
+        this.size--;
+
+        const value = this.q[this.first];
+        this.first++;
+
+        if (this.first === this.q.length) {
+            this.first = 0;
+        }
+
+        return value;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+}
